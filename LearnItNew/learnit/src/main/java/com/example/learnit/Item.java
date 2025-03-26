91f7ca4f809e3724e88d503c7464802825ec62d4
niesluchowski <156335856+niesluchowski@users.noreply.github.com>
2025-03-26 21:22:50 +0100

    Delete LearnItNew directory

--- LearnItNew/learnit/src/main/java/com/example/learnit/Item.java
+++
@@ -1,4 +0,0 @@
-package com.example.learnit;
-
-public class Item {
-}
--- LearnItNew/learnit/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.learnit {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.learnit to javafx.fxml;
-    exports com.example.learnit;
-}
